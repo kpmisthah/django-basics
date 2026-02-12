921612e24cda9d7fe4d327baac60dce7674f22c0
kpmisthah <kpmisthah6@gmail.com>
2026-02-12 23:07:32 +0530

    Refactor: Rename playground/url.py to playground/urls.py for convention

--- storefront/playground/urls.py
+++ storefront/playground/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 # url conf moduled
-urlPatterns = [
-    path('playground/hello',views.say_Hello)
+urlpatterns = [
+    path('hello/',views.say_Hello)
 ]
